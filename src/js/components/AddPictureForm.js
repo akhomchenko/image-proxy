84daed52e69076666c962466e13ce24ac133672e
Alex Khomchenko <akhomchenko@gmail.com>
2015-03-06 12:41:01 +0200

    add form action for picture form

--- src/js/components/AddPictureForm.js
+++ src/js/components/AddPictureForm.js
@@ -1,14 +1,25 @@
 import React from 'react/addons';
 
 export default React.createClass({
-  mixins: [React.addons.PureRenderMixin],
+  mixins: [React.addons.PureRenderMixin, React.addons.LinkedStateMixin],
+
+  getInitialState() {
+    return {
+      imageUrl: null
+    }
+  },
+
+  handleSubmit(e) {
+    e.preventDefault();
+  },
+
   render() {
     return (
       <div className="col-md-offset-4">
-        <form className="form-inline" role="form">
+        <form className="form-inline" role="form" onSubmit={this.handleSubmit}>
           <div className="form-group">
             <input type="text" placeholder="URL" required="true"
-              className="form-control"/>
+              className="form-control" valueLink={this.linkState('imageUrl')}/>
           </div>{' '} {/* this is ugly, I know */}
           <input type="submit" value="Load!"
             className="btn btn-lg btn-primary"/>
