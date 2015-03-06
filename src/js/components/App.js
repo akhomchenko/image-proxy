5863f06f5ad455351fc6817ef69cb813625ec600
Alex Khomchenko <akhomchenko@gmail.com>
2015-03-06 14:12:14 +0200

    listen to pictures store update

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,13 +1,15 @@
 import React from 'react/addons';
+import Reflux from 'reflux';
 import Picture from './Picture';
 import AddPictureForm from './AddPictureForm';
+import PicturesStore from '../stores/pictures-store';
 
 export default React.createClass({
-  mixins: [React.addons.PureRenderMixin],
+  mixins: [React.addons.PureRenderMixin, Reflux.connect(PicturesStore, 'picture')],
 
   getInitialState() {
     return {
-      image: null
+      picture: null
     }
   },
 
@@ -15,9 +17,9 @@ export default React.createClass({
     return (
       <div>
         <div className="page-header">
-        <AddPictureForm/>
-      </div>
-      <Picture image={this.state.image}/>
+          <AddPictureForm/>
+        </div>
+        <Picture image={this.state.picture}/>
       </div>
     );
   }
