65b30898b95922085afc668137ca5ae0564baf32
Alex Khomchenko <akhomchenko@gmail.com>
2015-03-25 13:16:44 +0200

    loading state for the button

--- src/js/components/AddPictureForm.js
+++ src/js/components/AddPictureForm.js
@@ -1,35 +1,43 @@
 import is from 'is_js';
-import React from 'react/addons';
+import cx from 'classnames';
 import Reflux from 'reflux';
+import React from 'react/addons';
 import {PicturesActions} from '../actions';
+
+const {PureRenderMixin, LinkedStateMixin} = React.addons;
+const {listenTo} = Reflux;
 
 export default React.createClass({
   mixins: [
-    React.addons.PureRenderMixin,
-    React.addons.LinkedStateMixin,
-    Reflux.listenTo(PicturesActions.add.failed, 'onAddFailed'),
-    Reflux.listenTo(PicturesActions.add.completed, 'onAddCompleted')
+    PureRenderMixin, LinkedStateMixin,
+    listenTo(PicturesActions.add.failed, 'onAddFailed'),
+    listenTo(PicturesActions.add.completed, 'onAddCompleted')
   ],
 
   getInitialState() {
-    return {imageUrl: null, errorMessage: null};
+    return {imageUrl: null, errorMessage: null, isLoading: false};
   },
 
   handleSubmit(e) {
     e.preventDefault();
     PicturesActions.add(this.state.imageUrl);
+    this.setState({isLoading: true});
   },
 
   onAddCompleted() {
-    this.setState({imageUrl: null, errorMessage: null});
+    this.setState({imageUrl: null, errorMessage: null, isLoading: false});
   },
 
   onAddFailed(error) {
     const errorMessage = (error || {}).error || 'without any reason';
-    this.setState({errorMessage});
+    this.setState({errorMessage, isLoading: false});
   },
 
   render() {
+    const isLoading = this.state.isLoading;
+    const loadButtonText = isLoading ? 'Loading...' : 'Load!';
+    const loadButtonStyles = cx('btn btn-lg btn-primary', {disabled: isLoading});
+
     return (
       <div className="row">
         <div className="col-md-offset-4">
@@ -38,8 +46,7 @@ export default React.createClass({
               <input type="text" placeholder="URL" required="true"
                 className="form-control" valueLink={this.linkState('imageUrl')}/>
             </div>{' '} {/* this is ugly, I know */}
-            <input type="submit" value="Load!"
-              className="btn btn-lg btn-primary"/>
+            <input type="submit" value={loadButtonText} className={loadButtonStyles}/>
           </form>
         </div>
         <br/>
